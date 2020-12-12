92d5f9c2c29710173a9b6a2ca6a847b9c55ce114
ahyaemon <ahyaemon>
2020-12-12 07:36:11 +0000

    Initial commit

---
+++ src/main/kotlin/com/ahyaemon/KabuCommand.kt
@@ -0,0 +1,30 @@
+package com.ahyaemon
+
+import io.micronaut.configuration.picocli.PicocliRunner
+import io.micronaut.context.ApplicationContext
+
+import picocli.CommandLine
+import picocli.CommandLine.Command
+import picocli.CommandLine.Option
+import picocli.CommandLine.Parameters
+
+@Command(name = "kabu", description = ["..."],
+        mixinStandardHelpOptions = true)
+class KabuCommand : Runnable {
+
+    @Option(names = ["-v", "--verbose"], description = ["..."])
+    private var verbose : Boolean = false
+
+    override fun run() {
+        // business logic here
+        if (verbose) {
+            println("Hi!")
+        }
+    }
+
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            PicocliRunner.run(KabuCommand::class.java, *args)
+        }
+    }
+}
